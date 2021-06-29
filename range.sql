baa76b898dfa0b09b1139129de85f781b8062f2e
Raghadsaud97 <86537522+Raghadsaud97@users.noreply.github.com>
2021-06-30 00:24:50 +0300

    Add files via upload

---
+++ range.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 29, 2021 at 11:23 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `task1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `range`
+--
+
+CREATE TABLE `range` (
+  `motor1` int(10) NOT NULL,
+  `motor2` int(10) NOT NULL,
+  `motor3` int(10) NOT NULL,
+  `motor4` int(10) NOT NULL,
+  `motor5` int(10) NOT NULL,
+  `motor6` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ task.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<head>
+<style>
+body {
+	background-color: #d1c4e9;
+	text-align: center;
+}
+</style>
+<body>
+<h1> محركات ذراع الروبوت</h1>
+
+
+<p>المحرك 1</p>
+<input type="range" name="R1" id="range11" value="10" min="1" max="100" oninput="rangeout1.value = range11.value">
+<output  id="rangeout1"></output>
+<br>
+  
+
+<p>المحرك 2</p>
+<input type="range" name="R2" id="range22" value="50" min="1" max="100" oninput="rangeout2.value = range22.value">
+<output  id="rangeout2"></output>
+<br>
+   
+<p>المحرك 3:</p>
+<input type="range" name="R3" id="range33" value="70" min="1" max="100" oninput="rangeout3.value = range33.value">
+<output  id="rangeout3"></output>
+<br>
+   
+<p>المحرك 4:</p>
+<input type="range" name="R4" id="range44" value="5" min="1" max="100" oninput="rangeout4.value = range44.value">
+<output  id="rangeout4"></output>
+<br>
+  
+<p>المحرك 5:</p>
+<input type="range" name="R5" id="range55" value="40" min="1" max="100" oninput="rangeout5.value = range55.value">
+<output  id="rangeout5"></output>
+<br>
+   
+<p>المحرك 6:</p>
+<input type="range" name="R6" id="range66" value="5" min="1" max="100" oninput="rangeout6.value = range66.value">
+<output  id="rangeout6"></output>
+<br>
+   
+   <button type="button" >حفظ</button>
+   <button type="button">تشغيل</button>
+   
+ 
+</body>
+</head>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
